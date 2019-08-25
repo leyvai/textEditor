60cea37842236f4a78a64bb76d8662e789f06753
leyvai <bu50.88@gmail.com>
2019-08-25 21:05:21 +0300

    works.. kinda

--- src/App.js
+++ src/App.js
@@ -8,13 +8,49 @@ const styles = {
 };
 
 class App extends Component {
+  state = {
+    color: "black",
+    fontWeight: "normal",
+    fontStyle: "null",
+    textDecorationLine: "null"
+  };
+
+  setColor = color => {
+    this.setState({ color: color });
+  };
+
+  setStyle = style => {
+    if (style === "bold") {
+      if (this.state.fontWeight === "normal") {
+        this.setState({ fontWeight: "bold" });
+      } else {
+        this.setState({ fontWeight: "normal" });
+      }
+    } else if (style === "italic") {
+      if (this.state.fontStyle === "normal") {
+        this.setState({ fontStyle: "italic" });
+      } else {
+        this.setState({ fontStyle: "normal" });
+      }
+    } else if (style === "underline") {
+      if (this.state.textDecorationLine === "normal") {
+        this.setState({ textDecorationLine: "underline" });
+      } else {
+        this.setState({ textDecorationLine: "normal" });
+      }
+    }
+  };
   render() {
     let styleNames = ["bold", "italic", "underline"];
     let colors = ["yellow", "blue", "red", "black", "purple"];
 
     let stylingBoxes = styleNames.map(style => {
       return (
-        <button style={styles[style]} key={style}>
+        <button
+          style={styles[style]}
+          key={style}
+          onClick={() => this.setStyle(style)}
+        >
           {style}
         </button>
       );
@@ -25,6 +61,7 @@ class App extends Component {
         <button
           style={{ backgroundColor: color, height: 30, width: 30 }}
           key={color}
+          onClick={() => this.setColor(color)}
         />
       );
     });
@@ -32,7 +69,14 @@ class App extends Component {
     return (
       <div className="App">
         <div className="my-3">{stylingBoxes}</div>
-        <textarea />
+        <textarea
+          style={{
+            color: this.state.color,
+            fontWeight: this.state.fontWeight,
+            fontStyle: this.state.fontStyle,
+            textDecorationLine: this.state.textDecorationLine
+          }}
+        />
         <div className="my-3">{colorBoxes}</div>
       </div>
     );
